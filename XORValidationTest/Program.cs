ec59b39bcaf68e2154dff81630aba0b964f9c1cf
Robert Vella <robert.r.h.vella@gmail.com>
2012-09-19 21:14:49 +0200

    Improve debugging information in XOR validation test. 1. Add no change rate constant. 2. Change value of disable gene constant. 3. Remove parallelism, as it was buggy. 4. Remove crossover rate. 5. Remove stale comments. 6. Show averages and current best at every generation.

--- XORValidationTest/Program.cs
+++ XORValidationTest/Program.cs
@@ -9,7 +9,7 @@ namespace XORValidationTest
 {
     public class Program
     {
-        private const int NUMBER_OF_RUNS = 2;
+        private const int NUMBER_OF_RUNS = 100;
         private static double OPTIMAL_SCORE = 16;
         private static int POPULATION_SIZE = 150;
 
@@ -23,46 +23,49 @@ namespace XORValidationTest
         private static double WEIGHT_MUTATION_RATE = 0.8;
         private static double NEW_NEURON_RATE = 0.03;
         private static double NEW_LINK_RATE = 0.05;
+        private static double NO_CHANGE_RATE = 0.12;
+
         private static double WEIGHT_PERTUBATION_RATE = 0.9;
-        private static double DISABLE_GENE_RATE = 0.12;
-        private static double MAX_PERTURBATION = 0.5;
+
+        private static double DISABLE_GENE_RATE = 0.75;
+
+        private static double MAX_PERTURBATION = 2;
         private static double MAX_WEIGHT = 5;
 
         private const double EXCESS_GENES_WEIGHT = 1.0;
         private const double DISJOINT_GENES_WEIGHT = 1.0;
         private const double MATCHING_GENES_WEIGHT = 0.4;
-        private const double FUNCTION_DIFF_WEIGHT = 0;
 
-        private const int SOLUTION_PRECISION = 4;
-
-        private static double CROSSOVER_RATE = 0.75;
+        private const int OUTPUT_ACTIVATION_PRECISION = 8;
+        private const int DEBUG_PRECISION = 8;
 
         
         public static void Main(string[] args)
         {
-            //Perform 100 tests, recording the average number of generations, hidden nodes for 
-            //the final network, and enabled genes for the final genome.
             var numberOfFailures = 0;
 
-            var averageMatingEvents = 0.0;
-            var averageHiddenNodes = 0.0;
-            var averageEnabledGenes = 0.0;
+            var totalMatingEvents = 0.0;
+            var totalHiddenNodes = 0.0;
+            var totalEnabledGenes = 0.0;
+
             var neuronCounts = new List<int>();
-            var runsCompleted = 0;
             
+            var averageMatingEvents = 0.0; 
+            var averageHiddenNodes = 0.0; 
+            var averageEnabledGenes = 0.0; 
+
+            var averageNeuronCount = 0.0;
+            var STDDEVNeuronCount = 0.0;
+
             Console.WriteLine(String.Format("0 out of {0} runs completed.", NUMBER_OF_RUNS));
 
-            Enumerable.Range(0, NUMBER_OF_RUNS).AsParallel().ForAll(delegate(int i) {
+            foreach(var i in Enumerable.Range(0, NUMBER_OF_RUNS)) 
+            {
                 //Initialise the GA.
-                var testGA = new CPPNNEATGA(2, POPULATION_SIZE, delegate(CPPNNEATGenome genome)
-                {
-                    var result = (from num in Enumerable.Range(0, 4) 
-                            select Math.Round(genome.Phenome.GetActivation(new double[] {num / 2, num % 2}), SOLUTION_PRECISION)).ToArray();
-                    return Math.Pow(4 - result.Zip(CORRECT_RESULT, (a, t) => Math.Abs(t - a)).Sum(), 2);
-                }, 
-                new List<Func<double,double>>() {
-                    CPPNActivationFunctions.SteepenedSigmoidActivationFunction
-                });
+                var testGA = new CPPNNEATGA(2, POPULATION_SIZE, FitnessFunction, 
+                    new List<Func<double,double>>() {
+                        CPPNActivationFunctions.SteepenedSigmoidActivationFunction
+                    });
 
                 testGA.InterSpeciesMatingRate = INTERSPECIES_MATING_RATE;
                 testGA.CompatibilityDistanceThreshold = COMPATIBILITY_DISTANCE_THRESHOLD;
@@ -71,17 +74,17 @@ namespace XORValidationTest
                 testGA.WeightMutationRate = WEIGHT_MUTATION_RATE;
                 testGA.NewNeuronRate = NEW_NEURON_RATE;
                 testGA.NewLinkRate = NEW_LINK_RATE;
+                testGA.NoChangeRate = NO_CHANGE_RATE;
+                
                 testGA.DisableGeneRate = DISABLE_GENE_RATE;
 
                 testGA.WeightPertubationRate = WEIGHT_PERTUBATION_RATE;
                 testGA.MaxPerturbation = MAX_PERTURBATION;
                 testGA.MaxWeight = MAX_WEIGHT;
-                testGA.CrossoverRate = CROSSOVER_RATE;
 
                 testGA.ExcessGenesWeight = EXCESS_GENES_WEIGHT;
                 testGA.DisjointGenesWeight = DISJOINT_GENES_WEIGHT;
                 testGA.MatchingGenesWeight = MATCHING_GENES_WEIGHT;
-                testGA.FunctionDifferenceWeight = FUNCTION_DIFF_WEIGHT;
 
                 testGA.Initialise();
 
@@ -89,13 +92,28 @@ namespace XORValidationTest
 
                 while (testGA.Best.Score < OPTIMAL_SCORE && !testGA.Failed)
                 {
-                    Console.WriteLine(testGA.Best.Score);
-
                     testGA.Iterate();
                     matingEvents++;
+
+                    if(matingEvents % MATING_EVENTS_PER_GENERATION == 0) 
+                    {
+                        Console.Clear();
+                        Console.WriteLine(String.Format("{0} out of {1} runs completed.", i, NUMBER_OF_RUNS));
+                        Console.WriteLine(matingEvents / MATING_EVENTS_PER_GENERATION + " generations completed for current run.");
+                        Console.WriteLine();
+                        Console.WriteLine(String.Format("Current fitness: {0:f2}", testGA.Best.Score));
+                        Console.WriteLine();
+                        Console.WriteLine("Average number of generations: " + averageMatingEvents / MATING_EVENTS_PER_GENERATION);
+                        Console.WriteLine("Average number of hidden nodes: " + averageHiddenNodes);
+                        Console.WriteLine("Average number of enabled genes: " + averageEnabledGenes);
+                        Console.WriteLine("Standard deviation of neuron count: " + STDDEVNeuronCount);
+                        Console.WriteLine();
+                        Console.WriteLine("Number of failures: " + numberOfFailures);
+                    }
                 }
                 
                 var best = testGA.Best;
+                var runsCompleted = i + 1;
 
                 if (testGA.Failed)
                 {
@@ -103,41 +121,44 @@ namespace XORValidationTest
                 }
                 else
                 {
-                    averageMatingEvents += matingEvents;
-                    averageHiddenNodes += (from hiddenNeuron in best.Phenome.Neurons where hiddenNeuron is CPPNHiddenNeuron select hiddenNeuron).Count();
-                    averageEnabledGenes += (from gene in best.GeneCollection.LinkGenes where gene.Enabled select gene).Count();
+                    totalMatingEvents += matingEvents;
 
-                    lock (neuronCounts)
-                    {
-                        neuronCounts.Add(best.GeneCollection.Phenome.Neurons.Count());
-                    }
+                    totalHiddenNodes += (from hiddenNeuron in best.Phenome.Neurons 
+                                         where hiddenNeuron is CPPNHiddenNeuron 
+                                         select hiddenNeuron).Count();
+
+                    totalEnabledGenes += (from gene in best.GeneCollection.LinkGenes 
+                                          where gene.Enabled 
+                                          select gene).Count();
+
+                    neuronCounts.Add(best.GeneCollection.Phenome.Neurons.Count());
                 }
 
-                runsCompleted++;
+                averageMatingEvents = totalMatingEvents / runsCompleted; 
+                averageHiddenNodes = totalHiddenNodes / runsCompleted; 
+                averageEnabledGenes = totalEnabledGenes / runsCompleted; 
 
-                Console.Clear();
-                Console.WriteLine(String.Format("{0} out of {1} runs completed.", runsCompleted, NUMBER_OF_RUNS));
-
-            });
-
-            averageMatingEvents /= NUMBER_OF_RUNS; 
-            averageHiddenNodes /= NUMBER_OF_RUNS; 
-            averageEnabledGenes /= NUMBER_OF_RUNS; 
-
-            var averageNeuronCount = neuronCounts.Average();
-            var STDDEVNeuronCount = Math.Sqrt(neuronCounts.Select(neuronCount =>
-                                                                Math.Pow(neuronCount - averageNeuronCount, 2))
-                                            .Average());
-
-            //Print the results to the user.
-            Console.WriteLine("Average number of generations: " + averageMatingEvents / MATING_EVENTS_PER_GENERATION);
-            Console.WriteLine("Average number of hidden nodes: " + averageHiddenNodes);
-            Console.WriteLine("Average number of enabled genes: " + averageEnabledGenes);
-            Console.WriteLine("Standard deviation of neuron count: " + STDDEVNeuronCount);
-            Console.WriteLine("Number of failures: " + numberOfFailures);
+                if (neuronCounts.Count > 0)
+                {
+                    averageNeuronCount = neuronCounts.Average();
+                    STDDEVNeuronCount = Math.Sqrt(neuronCounts.Select(neuronCount =>
+                                                    Math.Pow(neuronCount - averageNeuronCount, 2))
+                                                .Average());
+                }
+            }
 
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
+        }
+        
+        public static double FitnessFunction(CPPNNEATGenome genome) {
+            var result = (from num in Enumerable.Range(0, 4) 
+                    select Math.Round(genome.Phenome.GetActivation(new double[] {num / 2, num % 2}), 
+                                        OUTPUT_ACTIVATION_PRECISION)).ToArray();
+
+            return Math.Pow(4 - result.Zip(CORRECT_RESULT, 
+                                        (a, t) => Math.Abs(t - a))
+                                      .Sum(), 2);
         }
     }
 }
