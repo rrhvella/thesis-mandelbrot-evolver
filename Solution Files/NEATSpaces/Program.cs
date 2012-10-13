993c5c9d68826e743ca85c7c3a79442bfefaafc2
Robert Vella <robert.r.h.vella@gmail.com>
2012-10-13 17:46:42 +0200

    Include elitism rate and interspecies mating rate. Also: Use markov parameters for experiment.

--- Solution Files/NEATSpaces/Program.cs
+++ Solution Files/NEATSpaces/Program.cs
@@ -11,25 +11,28 @@ namespace NEATSpaces
 {
     class Program: MapForm<CPPNNEATGA, CPPNNEATGenome, CPPNNEATGeneCollection, CPPNNetwork>
     {
-        private static double COMPATIBILITY_DISTANCE_THRESHOLD = 3.0;
+        private static double COMPATIBILITY_DISTANCE_THRESHOLD = 4.0;
 
         private static int NO_INNOVATION_THRESHOLD = 1500;
 
         private static double WEIGHT_MUTATION_RATE = 0.8;
-        private static double NEW_NEURON_RATE = 0.03;
-        private static double NEW_LINK_RATE = 0.05;
+        private static double NEW_NEURON_RATE = 0.01;
+        private static double NEW_LINK_RATE = 0.3;
 
         private static double WEIGHT_PERTUBATION_RATE = 0.9;
 
         private static double DISABLE_GENE_RATE = 0.75;
 
-        private static double MAX_PERTURBATION = 1.25;
-        private static double MAX_WEIGHT = 2.5;
+        private static double MAX_PERTURBATION = 1.8;
+        private static double MAX_WEIGHT = 1.8;
 
         private const double EXCESS_GENES_WEIGHT = 1.0;
         private const double DISJOINT_GENES_WEIGHT = 1.0;
-        private const double MATCHING_GENES_WEIGHT = 0.4;
+        private const double MATCHING_GENES_WEIGHT = 3.0;
         private const double FUNCTION_DIFFERENCE_WEIGHT = 1.0;
+
+        private const double ELITISM_RATE = 0.4;
+        private const double INTERSPECIES_MATING_RATE = 0.001;
 
         private const int NUMBER_OF_INPUTS = 2;
 
@@ -92,6 +95,9 @@ namespace NEATSpaces
             result.NewLinkRate = NEW_LINK_RATE;
             
             result.DisableGeneRate = DISABLE_GENE_RATE;
+
+            result.ElitismRate = ELITISM_RATE;
+            result.InterSpeciesMatingRate = INTERSPECIES_MATING_RATE;
 
             result.WeightPertubationRate = WEIGHT_PERTUBATION_RATE;
             result.MaxPerturbation = MAX_PERTURBATION;
