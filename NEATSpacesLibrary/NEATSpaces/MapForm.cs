78d398d562fadd94a9ab3d623487820cd99e68ae
Robert Vella <robert.r.h.vella@gmail.com>
2012-10-21 18:43:57 +0200

    Use original experiment parameters in MapForm.

--- NEATSpacesLibrary/NEATSpaces/MapForm.cs
+++ NEATSpacesLibrary/NEATSpaces/MapForm.cs
@@ -18,15 +18,15 @@ namespace NEATSpacesLibrary.NEATSpaces
             where GAType : BaseGA<GenomeType, GType, PType>
             where GenomeType : Genome<GType, PType>, new()
     {
-        private const int NUMBER_OF_GENERATIONS = 10;
-        private const int NUMBER_OF_RUNS = 100;
+        private const int NUMBER_OF_GENERATIONS = 250;
+        private const int NUMBER_OF_RUNS = 1;
 
-        private const int MATING_EVENTS_PER_GENERATION = 20;
+        private const int MATING_EVENTS_PER_GENERATION = 2000;
         private const int TOTAL_TICKS = MATING_EVENTS_PER_GENERATION * NUMBER_OF_GENERATIONS;
 
         private const int NUMBER_OF_TICKS_TO_UPDATE_IMAGE = 10;
 
-        private const int POPULATION_SIZE = 10;
+        private const int POPULATION_SIZE = 120;
 
         public MapForm(): base(NUMBER_OF_GENERATIONS, NUMBER_OF_RUNS, MATING_EVENTS_PER_GENERATION, NUMBER_OF_TICKS_TO_UPDATE_IMAGE,
                             POPULATION_SIZE)
