bff480845f84daad70e1961e731d3821f4d1f862
Robert Vella <robert.r.h.vella@gmail.com>
2012-11-13 19:49:32 +0100

    Escape version.

---
+++ Solution Files/ComplexCPPNNEATSelection/GAPictureBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NEATSpacesLibrary.GeneticAlgorithms;
+using System.Drawing;
+using NEATSpacesLibrary.CPPNNEAT;
+using System.Numerics;
+
+namespace NEATSpacesLibrary.NEATSpaces
+{
+    public class GAPanel: Panel
+    {
+        public CPPNNEATGenome Genome
+        {
+            get;
+            set;
+        }
+
+        public int Score
+        {
+            get;
+            set;
+        }
+
+        public GAPanel(int zoomFactor)
+        {
+            this.zoomFactor = zoomFactor;
+
+            Click += new EventHandler(GAPictureBox_Click);
+            Paint += new PaintEventHandler(GAPanel_Paint);
+        }
+
+
+        private const int ESCAPE = 50;
+        private int zoomFactor;
+
+        void GAPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (Genome != null && Genome.Phenome != null)
+            {
+                var image = new Bitmap(Width / zoomFactor, Height / zoomFactor);
+                var network = Genome.Phenome;
+                network.Reset();
+
+                var graphics = e.Graphics;
+
+                foreach (var x in Enumerable.Range(0, image.Width))
+                {
+                    foreach (var y in Enumerable.Range(0, image.Height))
+                    {
+                        var complex = network.GetActivation(new Complex[] { new Complex((double)x / image.Width, 
+                                                                                (double)y / image.Height)});
+                        var currentMagnitude = complex.Magnitude; 
+                        int i = 0;
+
+                        for(; i < ESCAPE && currentMagnitude < 4; i++) { 
+                            complex = network.GetActivation(new Complex[] { complex });
+                            currentMagnitude = complex.Magnitude; 
+                        }
+
+                        var intensity = (int)((double)i / ESCAPE * 255);
+                        image.SetPixel(x, y, Color.FromArgb(intensity, intensity, intensity));
+                    }
+                }
+
+                graphics.DrawImage(image, new RectangleF(0, 0, Width, Height));
+            }
+        }
+
+        private void GAPictureBox_Click(object sender, EventArgs e)
+        {
+            Score++;
+        }
+    }
+}
---
+++ Solution Files/ComplexCPPNNEATSelection/ImageSelectionForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NEATSpacesLibrary.NEATSpaces;
+using NEATSpacesLibrary.GeneticAlgorithms;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using NEATSpacesLibrary.CPPNNEAT;
+using System.Numerics;
+
+namespace NEATSpacesLibrary.NEATSpaces
+{
+    public class NEATImageSelectionForm: Form
+    {
+        private static double COMPATIBILITY_DISTANCE_THRESHOLD = 3.0;
+
+        private const double INPUT_DIVISOR = 30;
+
+        private static int NO_INNOVATION_THRESHOLD = 1000000;
+        private static int ITERATIONS_TO_CLEAR_LINK_CACHE = 1;
+
+        private static double WEIGHT_MUTATION_RATE = 0.9;
+        private static double NEW_NEURON_RATE = 0.1;
+        private static double NEW_LINK_RATE = 0.2;
+
+        private static double WEIGHT_PERTUBATION_RATE = 0.9;
+
+        private static double DISABLE_GENE_RATE = 0.75;
+
+        private static double MAX_PERTURBATION = 2.0;
+        private static double MAX_WEIGHT = MAX_PERTURBATION;
+
+        private const double EXCESS_GENES_WEIGHT = 1.0;
+        private const double DISJOINT_GENES_WEIGHT = 1.0;
+        private const double MATCHING_GENES_WEIGHT = 0.4;
+        private const double FUNCTION_DIFFERENCE_WEIGHT = 1.0;
+
+        private const double ELITISM_RATE = 0.3;
+        private const double INTERSPECIES_MATING_RATE = 0.001;
+        
+        private const double CROSSOVER_RATE = 0.75;
+
+        private const int NUMBER_OF_INPUTS = 1;
+
+        private int imagesPerRow = 3;
+        private int imageWidth = 50;
+        private int populationSize = 9;
+        private int imageHeight = 50;
+        private int zoomFactor = 4;
+
+        private GAPanel[] images;
+        private CPPNNEATGA ga;
+
+        public NEATImageSelectionForm()
+        {
+            this.ClientSize = new Size(imagesPerRow * imageWidth * zoomFactor, 
+                            (int)(Math.Ceiling(populationSize / (float)imagesPerRow) * imageHeight * zoomFactor));
+
+            images = new GAPanel[populationSize];
+
+            foreach (var i in Enumerable.Range(0, populationSize))
+            {
+                var newPictureBox = new GAPanel(zoomFactor);
+
+                newPictureBox.Width = imageWidth * zoomFactor;
+                newPictureBox.Height = imageHeight * zoomFactor;
+
+                newPictureBox.Left = imageWidth * (i % imagesPerRow) * zoomFactor;
+                newPictureBox.Top = imageWidth * (i / imagesPerRow) * zoomFactor;
+
+                images[i] = newPictureBox;
+
+                Controls.Add(newPictureBox);
+                newPictureBox.Show();
+            }
+
+            this.KeyDown += new KeyEventHandler(ImageSelectionForm_KeyDown);
+
+            this.ga = new CPPNNEATGA(NUMBER_OF_INPUTS, populationSize,
+                                        delegate(CPPNNEATGenome genome)
+                                        {
+                                            var pictureBox = images.Where(image => image.Genome == genome).FirstOrDefault();
+                                            return (pictureBox != null)? pictureBox.Score : 0;
+                                        }, new List<Func<Complex,Complex>>  {
+                                                CPPNActivationFunctions.ComplexGaussianActivationFunction,
+                                                CPPNActivationFunctions.ComplexSteepenedSigmoidActivationFunction,
+                                                CPPNActivationFunctions.ComplexSinActivationFunction,
+                                                CPPNActivationFunctions.ComplexLinearActivationFunction,
+                                                },
+                                                CPPNActivationFunctions.ComplexLinearActivationFunction,
+                                        true);
+
+            ga.CompatibilityDistanceThreshold = COMPATIBILITY_DISTANCE_THRESHOLD;
+            ga.NoInnovationThreshold = NO_INNOVATION_THRESHOLD;
+            ga.IterationsToClearLinkCache = ITERATIONS_TO_CLEAR_LINK_CACHE;
+
+            ga.WeightMutationRate = WEIGHT_MUTATION_RATE;
+            ga.NewNeuronRate = NEW_NEURON_RATE;
+            ga.NewLinkRate = NEW_LINK_RATE;
+
+            ga.DisableGeneRate = DISABLE_GENE_RATE;
+
+            ga.ElitismRate = ELITISM_RATE;
+            ga.InterSpeciesMatingRate = INTERSPECIES_MATING_RATE;
+
+            ga.WeightPertubationRate = WEIGHT_PERTUBATION_RATE;
+            ga.MaxPerturbation = MAX_PERTURBATION;
+            ga.MaxWeight = MAX_WEIGHT;
+
+            ga.CrossoverRate = CROSSOVER_RATE;
+
+            ga.ExcessGenesWeight = EXCESS_GENES_WEIGHT;
+            ga.DisjointGenesWeight = DISJOINT_GENES_WEIGHT;
+            ga.MatchingGenesWeight = MATCHING_GENES_WEIGHT;
+            ga.FunctionDifferenceWeight = FUNCTION_DIFFERENCE_WEIGHT;
+
+            ga.Initialise();
+            LoadGenomesIntoImages();
+        }
+
+        private void ImageSelectionForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ga.ForceUpdateGenomes();
+                ga.GenerationalIterate();
+                LoadGenomesIntoImages();
+            }
+        }
+
+        private void LoadGenomesIntoImages()
+        {
+            var index = 0;
+
+            foreach (var genome in ga.Population.OrderByDescending(genome => genome.Score))
+            {
+                images[index].Score = 0;
+                images[index++].Genome = genome;
+            }
+
+            Refresh();
+        }
+
+        public static void Main(String[] args)
+        {
+            Application.Run(new NEATImageSelectionForm());
+        }
+    }
+}
