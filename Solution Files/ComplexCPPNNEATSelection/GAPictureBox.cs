8d0360afc1d72b5287e05a7c5cc3246d52c1cd85
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-02 13:20:25 +0100

    Use Ashlock's colouring method.

--- Solution Files/ComplexCPPNNEATSelection/GAPictureBox.cs
+++ Solution Files/ComplexCPPNNEATSelection/GAPictureBox.cs
@@ -37,7 +37,7 @@ namespace NEATSpacesLibrary.NEATSpaces
 
         private const int ESCAPE = 100;
         private const double MIN = -2;
-        private const double MAX = 2;
+        private const double MAX = 1;
         private int zoomFactor;
 
         private const double ESCAPE_MAGNITUDE = 2;
@@ -48,6 +48,14 @@ namespace NEATSpacesLibrary.NEATSpaces
         private const int BLUE_OFFSET = 0;
 
         private const int BYTES_PER_INT = 4;
+
+        private const double ARED = 0.019;
+        private const double AGREEN = 0.015;
+        private const double ABLUE = 0.011;
+
+        private const double BRED = 0.8;
+        private const double BGREEN = 0.8;
+        private const double BBLUE = 0.8;
 
         void GAPanel_Paint(object sender, PaintEventArgs e)
         {
@@ -77,6 +85,7 @@ namespace NEATSpacesLibrary.NEATSpaces
                         for(; i < ESCAPE && currentMagnitude < ESCAPE_MAGNITUDE; i++) { 
                             complex = network.GetActivation(new Complex[] { positionComplex, complex });
                             currentMagnitude = complex.Magnitude; 
+
                         }
 
                         Marshal.WriteInt32(drawingBuffer.Scan0 + drawingBuffer.Stride * y + x * BYTES_PER_INT, ToColour(i));
@@ -91,9 +100,12 @@ namespace NEATSpacesLibrary.NEATSpaces
 
         private int ToColour(int iterationNumber)
         {
-            var intensity = (iterationNumber * 255) / ESCAPE;
-            return 255 << ALPHA_OFFSET | intensity << RED_OFFSET | 
-                    intensity << GREEN_OFFSET | intensity << BLUE_OFFSET;
+            var r = 127 + (int)(128 * Math.Cos(ARED * iterationNumber + BRED));
+            var g = 127 + (int)(128 * Math.Cos(AGREEN * iterationNumber + BGREEN));
+            var b = 127 + (int)(128 * Math.Cos(ABLUE * iterationNumber + BBLUE));
+
+            return 255 << ALPHA_OFFSET | r << RED_OFFSET | 
+                    g << GREEN_OFFSET | b << BLUE_OFFSET;
         }
 
         private void GAPictureBox_Click(object sender, EventArgs e)
