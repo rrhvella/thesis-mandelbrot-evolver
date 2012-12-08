e8179bc98a7428721127e9261c31b61f65de1308
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-08 15:13:58 +0100

    Extend serialization info to create generic GetValue method.

---
+++ NEATSpacesLibrary/Extensions/SerializationInfoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace NEATSpacesLibrary.Extensions
+{
+    public static class SerializationInfoExtensions
+    {
+        public static T GetValue<T>(this SerializationInfo self, string name)
+        {
+            return (T)self.GetValue(name, typeof(T));
+        }
+    }
+}
