a4eb1157fd392caf60f88065d9cc5f172cecf1ba
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-08 16:40:11 +0100

    Implement factories as class methods -- no need to have them returned by a property.

--- NEATSpacesLibrary/CPPNNEAT/CPPNNEATActivationFunction.cs
+++ NEATSpacesLibrary/CPPNNEAT/CPPNNEATActivationFunction.cs
@@ -12,91 +12,55 @@ namespace NEATSpacesLibrary.CPPNNEAT
         public const int MIN_POWER = 2;
         public const int MAX_POWER = 4;
 
-        public static Func<CPPNNEATActivationFunction> ComplexLinearActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexLinearActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => x, "x"); };
-            }
+            return new CPPNNEATActivationFunction(x => x, "x"); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexPolynomialActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexPolynomialActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() 
-                { 
-                    var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
-                    return new CPPNNEATActivationFunction(x => Complex.Pow(x, power), 
-                                                                        String.Format("x^{0}", power)); 
-                };
-            }
+            var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
+            return new CPPNNEATActivationFunction(x => Complex.Pow(x, power), 
+                                                                String.Format("x^{0}", power)); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexLogarithmicActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexLogarithmicActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() 
-                { 
-                    var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
-                    return new CPPNNEATActivationFunction(x => Complex.Log(x, power), 
-                                                                        String.Format("log{0}(x)", power)); 
-                };
-            }
+            var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
+            return new CPPNNEATActivationFunction(x => Complex.Log(x, power), 
+                                                                String.Format("log{0}(x)", power)); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexExponentialActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexExponentialActivationFunctionFactory()
         {
-            get
-            {
-                return delegate()
-                {
-                    var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
-                    return new CPPNNEATActivationFunction(x => Complex.Pow(power, x),
+            var power = MathExtensions.RandomInteger(MIN_POWER, MAX_POWER);
+            return new CPPNNEATActivationFunction(x => Complex.Pow(power, x),
                                                                         String.Format("{0}^x", power));
-                };
-            }
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexSteepenedSigmoidActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexSteepenedSigmoidActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => 1 / (1 + Complex.Exp(-4.9 * x)), "σ(x)"); };
-            }
+            return new CPPNNEATActivationFunction(x => 1 / (1 + Complex.Exp(-4.9 * x)), "σ(x)"); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexGaussianActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexGaussianActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => Complex.Exp(-Complex.Pow(x * 2.5, 2.0)), "∮(x)"); };
-            }
+            return new CPPNNEATActivationFunction(x => Complex.Exp(-Complex.Pow(x * 2.5, 2.0)), "∮(x)"); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexSinActivationFunctionFactory
+        public static CPPNNEATActivationFunction ComplexSinActivationFunctionFactory()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => Complex.Sin(x), "sin(x)"); };
-            }
+            return new CPPNNEATActivationFunction(x => Complex.Sin(x), "sin(x)"); 
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexEulerActivationFunctionFactory 
+        public static CPPNNEATActivationFunction ComplexEulerActivationFunctionFactory ()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => Complex.Exp(x), "e^x"); };
-            }
+            return new CPPNNEATActivationFunction(x => Complex.Exp(x), "e^x");
         }
 
-        public static Func<CPPNNEATActivationFunction> ComplexTanHActivationFunctionFactory 
+        public static CPPNNEATActivationFunction ComplexTanHActivationFunctionFactory ()
         {
-            get
-            {
-                return delegate() { return new CPPNNEATActivationFunction(x => Complex.Tanh(x), "tanh(x)"); };
-            }
+            return new CPPNNEATActivationFunction(x => Complex.Tanh(x), "tanh(x)"); 
         }
     }
 
