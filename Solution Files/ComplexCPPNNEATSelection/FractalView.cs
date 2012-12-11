bfd9a4458a81dd6fb9e7064c4ac85366cf66826f
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-11 22:48:22 +0100

    Fractal view comments.

--- Solution Files/ComplexCPPNNEATSelection/FractalView.cs
+++ Solution Files/ComplexCPPNNEATSelection/FractalView.cs
@@ -14,10 +14,21 @@ using MandelbrotCPPNNEAT;
 
 namespace ComplexCPPNNEATSelection
 {
+    /// <summary>
+    /// Draws the view of a Mandelbrot fractal in the complex plane, with the membership based on a CPPN-NEAT genome.
+    /// </summary>
     public class FractalView: Panel
     {
+        /// <summary>
+        /// The number of iterations performed before a Network is judged to tend to infinity.
+        /// </summary>
         private const int ESCAPE = 200;
 
+
+        /// <summary>
+        /// The resolution width, in pixels, of the fractal views. Resolution here refers to the number of complex numbers sampled 
+        /// within a view.
+        /// </summary>
         private int viewWidth;
         public int ViewWidth
         {
@@ -32,6 +43,10 @@ namespace ComplexCPPNNEATSelection
             }
         }
 
+        /// <summary>
+        /// The resolution height, in pixels, of the fractal views. Resolution here refers to the number of complex numbers sampled 
+        /// within a view.
+        /// </summary>
         private int viewHeight;
         public int ViewHeight
         {
@@ -46,6 +61,9 @@ namespace ComplexCPPNNEATSelection
             }
         }
 
+        /// <summary>
+        /// The genome on which the view is based.
+        /// </summary>
         private MandelbrotCPPNNEATGenome genome;
         public MandelbrotCPPNNEATGenome Genome
         {
@@ -60,15 +78,24 @@ namespace ComplexCPPNNEATSelection
             }
         }
 
+        /// <summary>
+        /// The score of the genome based on the number of user clicks.
+        /// </summary>
         public int Score
         {
             get;
             set;
         }
 
-
-        private Bitmap fractalImage;
+        /// <summary>
+        /// Is true if the image needs to be regenerated.
+        /// </summary>
         private bool fractalImageCacheInvalidated;
+
+        /// <summary>
+        /// The image containing the rendered view.
+        /// </summary>
+        private Bitmap fractalImage;
         public Image FractalImage
         {
             get
@@ -83,6 +110,9 @@ namespace ComplexCPPNNEATSelection
             }
         }
 
+        /// <summary>
+        /// Fired when the user selects this view. 
+        /// </summary>
         public event EventHandler<EventArgs> Selected;
 
         public FractalView()
@@ -94,20 +124,29 @@ namespace ComplexCPPNNEATSelection
         }
 
 
+        /// <summary>
+        /// Handles the event fired when the control is painted.
+        /// </summary>
         void GAPanel_Paint(object sender, PaintEventArgs e)
         {
+            //If a genome is registered, draw it's corresponding image.
             if (Genome != null && Genome.Phenome != null)
             {
                 e.Graphics.DrawImage(FractalImage, ClientRectangle);
             }
         }
 
+        /// <summary>
+        /// Handles the event fired when the control is clicked on.
+        /// </summary>
         private void GAPictureBox_Click(object sender, MouseEventArgs e)
         {
+            //Left button increases the score.
             if (e.Button == MouseButtons.Left)
             {
                 Score++;
             }
+            //Right button selects the view.
             else if (e.Button == MouseButtons.Right)
             {
                 Selected(this, new EventArgs());
