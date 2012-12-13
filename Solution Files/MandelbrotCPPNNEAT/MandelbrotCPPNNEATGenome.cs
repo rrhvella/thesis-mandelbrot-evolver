319ec4ceec27c58bc584714eb0512085586a8230
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-13 15:02:44 +0100

    Comment MandelbrotCPPNNEATGenome.cs

--- Solution Files/MandelbrotCPPNNEAT/MandelbrotCPPNNEATGenome.cs
+++ Solution Files/MandelbrotCPPNNEAT/MandelbrotCPPNNEATGenome.cs
@@ -10,7 +10,9 @@ using MandelbrotCPPNNEAT;
 
 namespace MandelbrotCPPNNEAT
 {
-
+    /// <summary>
+    /// The genome of an individual inside a Mandelbrot CPPN-NEAT GA <seealso cref="MandelbrotCPPNNEATGA"/>
+    /// </summary>
     public class MandelbrotCPPNNEATGenome: CPPNNEATGenome<CPPNNEATGeneCollection,
                                                             MandelbrotCPPNNEATPhenome>
     {
@@ -19,17 +21,31 @@ namespace MandelbrotCPPNNEAT
         {
         }
 
+        /// <summary>
+        /// Initialises an individual based on the genomes of its parents.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="partner"></param>
         public MandelbrotCPPNNEATGenome(MandelbrotCPPNNEATGenome parent, MandelbrotCPPNNEATGenome partner)
             :base(parent, partner)
         {
         }
 
+        /// <summary>
+        /// Returns the phenome of the individual <seealso cref="MandelbrotCPPNNEATPhenome"/>
+        /// </summary>
+        /// <returns></returns>
         protected override MandelbrotCPPNNEATPhenome GetPhenome()
         {
             var parent = Parent as MandelbrotCPPNNEATGA;
             return new MandelbrotCPPNNEATPhenome(GetNetwork(), parent.ViewPosition, parent.ViewScale);
         }
 
+        /// <summary>
+        /// Returns the child of this genome when it mates with another individual.
+        /// </summary>
+        /// <param name="partner"></param>
+        /// <returns></returns>
         protected override Genome<CPPNNEATGeneCollection, MandelbrotCPPNNEATPhenome> 
                 InnerCrossover(Genome<CPPNNEATGeneCollection, MandelbrotCPPNNEATPhenome> partner)
         {
