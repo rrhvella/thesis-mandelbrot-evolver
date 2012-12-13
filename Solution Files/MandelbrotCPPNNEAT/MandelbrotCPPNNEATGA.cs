61e7c792c631e17307b779c52d8ea3a1d48b0676
Robert Vella <robert.r.h.vella@gmail.com>
2012-12-13 15:02:30 +0100

    Comment MandelbrotCPPNNEATGA.cs

--- Solution Files/MandelbrotCPPNNEAT/MandelbrotCPPNNEATGA.cs
+++ Solution Files/MandelbrotCPPNNEAT/MandelbrotCPPNNEATGA.cs
@@ -14,22 +14,40 @@ using MandelbrotCPPNNEAT;
 namespace MandelbrotCPPNNEAT
 {
 
+    /// <summary>
+    /// Evolves new Mandelbrot sets based on the CPPN-NEAT algorithm <refer>.
+    /// </summary>
     public class MandelbrotCPPNNEATGA : BaseCPPNNEATGA<MandelbrotCPPNNEATGenome, CPPNNEATGeneCollection,
                                                         MandelbrotCPPNNEATPhenome>, ICPPNNEATGA
     {
         private const int NUMBER_OF_INPUTS = 2;
+
+        /// <summary>
+        /// The complex number at the top-left corner of the view.
+        /// </summary>
         public Complex ViewPosition
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The distance, in the complex plane, between the two corners of each side of the view.
+        /// </summary>
         public double ViewScale
         {
             get;
             set;
         }
-
+        
+        /// <summary>
+        /// Initialises a new Mandelbrot CPPN-NEAT GA.
+        /// </summary>
+        /// <param name="populationSize">The number of individuals in the population.</param>
+        /// <param name="scoreFunction">The function used to determine the fitness of an individual.</param>
+        /// <param name="canonicalFunctionList">The list of function factories used to generate the activation functions 
+        /// for the hidden inputs.</param>
+        /// <param name="outputActivationFunction">The function factory used to generate the output activation function. </param>
         public MandelbrotCPPNNEATGA(int populationSize, 
                         Func<MandelbrotCPPNNEATGenome, double> scoreFunction,
                         List<Func<CPPNNEATActivationFunction>> canonicalFunctionList, 
